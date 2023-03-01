ed859cd8560a12c7660de3e937b2cd3187ff90c4
Foont <dzinoviev@ukr.net>
2023-03-01 17:38:14 +0200

    1commit

---
+++ variables_and_strict_mode.js
@@ -0,0 +1,34 @@
+"use strict"; // strict mode
+
+// a = 15;
+// console.log(a); not working with use strict mode
+
+let number = 123;
+let Number = 1234;
+const leftBorderWidth = 12345;
+console.log(leftBorderWidth, number, Number);
+
+const obj = {
+    a:50,
+    b:50,
+}
+
+obj.a = 20;
+console.log(obj.a)
+
+
+/* HOISTING  */
+console.log(name) // return undefined (hoisting всплыьие переменных)
+var name = 'Ivan'; //not preferable
+
+name = 'Alex';
+
+{
+    let result = 50;
+    console.log(result) // area of visibility (working)
+}
+
+// console.log(result) // area of visibility variables (error)
+
+
+
